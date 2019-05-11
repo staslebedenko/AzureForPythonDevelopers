fde4c95b73c2084a2e591bc844fdc87244cb762d
Stas Lebedenko <lebedenko.stas@gmail.com>
2019-05-11 19:23:37 +0300

    Configuration changes for app publish

--- 02_Django_Project_Configured/AzurePythonBootcampWeb/AzurePythonBootcampWeb/settings.py
+++ 02_Django_Project_Configured/AzurePythonBootcampWeb/AzurePythonBootcampWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a^du6k_@45t@+0i&=iku$g-br6k3ghegzmdjz+#t7w9g9g#!$$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.azurewebsites.net', '127.0.0.1'] if 'WEBSITE_SITE_NAME' in os.environ else []
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
